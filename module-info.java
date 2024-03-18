96d10eebf23037f3e2e185257983c58cff4f0576
SauceSL <162396199+SauceSL@users.noreply.github.com>
2024-03-18 17:37:10 -0600

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,16 @@
+module topicosap.proyecto3r {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens topicosap.proyecto3r to javafx.fxml;
+    exports topicosap.proyecto3r;
+}
